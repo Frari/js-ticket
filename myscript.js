e40495389f511ea0492b5a4c13141b4d92e6084b
Frari <francescoriccardi86@gmail.com>
2019-04-16 16:15:04 +0200

    fine

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
-var kilometri = prompt ('Quanti kilometri devi percorrere?');
-var età = prompt ('Quanti anni hai?');
+var kilometri = prompt ("Quanti kilometri devi percorrere?");
+var eta = prompt ("Quanti anni hai?");
 var prezzo_km = 0.21;
-if (età < 18){
-  document.writeln((prezzo_km * kilometri)*0.8)
-}if else (età >= 65){
-  document.writeln((prezzo_km * kilometri)*0.6)
+if (eta < 18){
+  document.writeln("Il prezzo per il tuo viaggio è di:"+" "+(prezzo_km * kilometri)*0.8+" "+"Euro")
+}else if (eta >= 65){
+  document.writeln("Il prezzo per il tuo viaggio è di:"+" "+(prezzo_km * kilometri)*0.6+" "+"Euro")
 }else {
-  document.writeln(prezzo_km * kilometri)
+  document.writeln("Il prezzo per il tuo viaggio è di:"+" "+prezzo_km * kilometri+" "+"Euro")
 }
